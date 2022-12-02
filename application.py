55c5ab3a9942486ee0a33538f197e04d5b182a5c
fierceray <shengbiao.lei@gmail.com>
2022-12-02 13:13:51 -0500

    change app.py to application.py to commit to AWS

--- application.py
+++ application.py
@@ -2,27 +2,27 @@ from flask import Flask, render_template, request
 
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/leisure')
+@application.route('/leisure')
 def get_contact():
     return render_template('contact.html')
 
-@app.route('/resume')
+@application.route('/resume')
 def get_resume():
     return render_template('resume.html')
 
-@app.route('/cert')
+@application.route('/cert')
 def get_cert():
     return render_template('certificate.html')
 
-@app.route('/project')
+@application.route('/project')
 def get_project():
     return render_template('project.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
